6ccb7da53c952f6c67feac1b4eb43f5173f947a1
Mike-205 <mikemwongela1025@gmail.com>
2024-07-18 12:10:05 +0300

    created sign up xml

---
+++ app/src/main/java/com/example/vazismart/Activity/SignUpActivity.java
@@ -0,0 +1,24 @@
+package com.example.vazismart.Activity;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.vazismart.R;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if(getSupportActionBar() != null){
+            getSupportActionBar().hide();
+        }
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
