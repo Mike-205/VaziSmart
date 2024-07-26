6b706278c68ae80b1f514e631a2f187b905c4774
Mike-205 <mikemwongela1025@gmail.com>
2024-07-26 11:03:52 +0300

    added firebase and amended files

---
+++ app/src/main/java/Utils/FirestoreConstants.java
@@ -0,0 +1,8 @@
+package Utils;
+
+public class FirestoreConstants {
+    public static final String ITEMCOLLECTION = "items";
+    public static final String BANNERCOLLECTION = "App Banners";
+    public static final String BRANDS = "Brands";
+    public static final String REVIEWS = "Reviews";
+}
